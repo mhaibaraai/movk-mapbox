95d549202ffa3c22144186139da842d8d1497287
mhaibaraai <mhaibaraai@gmail.com>
2026-06-10 21:57:15 +0800

    build(play): 预构建 mapbox-gl 等 CJS 依赖，避免 vite 二次优化重载

--- playgrounds/play/nuxt.config.ts
+++ playgrounds/play/nuxt.config.ts
@@ -3,6 +3,17 @@ export default defineNuxtConfig({
   devtools: { enabled: true },
   css: ['~/assets/css/main.css'],
   compatibilityDate: 'latest',
+  vite: {
+    optimizeDeps: {
+      include: [
+        '@mapbox/mapbox-gl-draw',
+        '@movk/core',
+        '@vueuse/core',
+        'gcoord',
+        'mapbox-gl' // CJS
+      ]
+    }
+  },
   mapbox: {
     accessToken: process.env.NUXT_MAPBOX_TOKEN || '',
     tiandituToken: process.env.NUXT_TIANDITU_TOKEN || ''
