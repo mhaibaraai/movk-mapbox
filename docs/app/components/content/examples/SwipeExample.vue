59a2640bfd8224616af35e95cde1de042e27aa01
mhaibaraai <mhaibaraai@gmail.com>
2026-09-24 15:28:33 +0800

    docs(components): 新增 MaplibreSwipe 文档与示例并补充 useMapSync 按钮开启用法

---
+++ docs/app/components/content/examples/SwipeExample.vue
@@ -0,0 +1,15 @@
+<script setup lang="ts">
+const position = ref(0.5)
+</script>
+
+<template>
+  <div class="h-115 w-full overflow-hidden rounded-(--ui-radius) border border-default">
+    <MaplibreMap :options="{ style: 'https://tiles.openfreemap.org/styles/liberty', center: [116.397, 39.908], zoom: 11 }">
+      <MaplibreNavigationControl position="top-right" />
+      <MaplibreScaleControl position="bottom-left" />
+      <MaplibreSwipe v-model:position="position">
+        <MaplibreTiandituLayer layer="img" annotation />
+      </MaplibreSwipe>
+    </MaplibreMap>
+  </div>
+</template>
